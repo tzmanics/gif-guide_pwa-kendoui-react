4ca1901eefaf3c30415def62c00ec06c5fa9b193
tzmanics <tzmanics@gmail.com>
2018-03-13 07:03:54 -0400

    Add event functions and habits list

--- src/App.js
+++ src/App.js
@@ -5,15 +5,65 @@ import './App.css';
 import '@progress/kendo-theme-default/dist/all.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      habitId: 0,
+      habitName: '',
+      habitIteration: 0,
+      habits: []
+    };
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ habitName: event.target.value });
+  }
+
+  handleIterationChange = (event) => {
+    this.setState({ habitIteration: event.target.value });
+  }
+
+  handleAddHabit = (event) => {
+    this.setState({
+      habits: this.state.habits.concat([{
+        key: this.state.habitId,
+        name: this.state.habitName,
+        iteration: this.state.habitIteration
+      }]),
+      habitId: this.state.habitId + 1
+    });
+  }
+
   render() {
     return (
       <div className='App'>
+      <h1> Goals &amp; Iterations </h1>
+      <div className='habits-list'>
+        <ul>
+          {this.state.habits.map((habit, index) => [
+            <li key={index}>
+              <p> {habit.name} </p>
+              <div className='iterations-area'>
+                {[...Array(habit.iteration)].map((iteration, index) => {
+                  return <input key={index} type='radio' />
+                })}
+              </div>
+            </li>
+          ])}
+        </ul>
+      </div>
         <div className='add-habit'>
           <label> GOAL: </label>
-          <input />
+          <input type='text' onChange={this.handleNameChange} />
           <label> ITERATION </label>
-          <NumericTextBox />
-          <Button>
+          <NumericTextBox
+            format='0'
+            min={0}
+            max={22}
+            onChange={this.handleIterationChange}
+          />
+          <Button primary={true} onClick={this.handleAddHabit}>
+            Add Habit
           </Button>
         </div>
       </div>
